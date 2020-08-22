f4504c17430aff245cf997efddc3676ef0d8f4b3
shusweela <shusweela@gmailcom>
2020-08-22 02:38:36 -0500

    updating domJS assignment

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 //object created of favorite book
 var book = {title: "Avoiding Mr. Wrong--and what to Do If You Didn't--Ten Men who Will Ruin Your Life", authName: "Stephen Arterburn, Meg J. Rinck, Margaret Josephson Rinck", genre: "self-help or spiritual, religion", shrtDsrp: "descriptions of diffent categories of men women may fall for; and quizzes and more to also help women to understand themselves and guide into act more wisely in pursuit of Mr. Right--instead of contiuous crushed relationships", price: "14.92", seller: "Amazon",}
 
-Function value () {
+function value () {
     buyNow (price " " seller);
     learnMore (Title, " ",genre, " " shrtDescrp);
     console.log(book);
