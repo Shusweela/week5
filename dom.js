c7aed158e3b747c04ae03bba93f2a1d8b5a9c8e7
shusweela <shusweela@gmailcom>
2020-09-10 17:02:12 -0500

    on the way to completion

--- dom.js
+++ dom.js
@@ -1,5 +1,10 @@
 //Changes each element in first list of items. 
-
 document.getElementById("cat").style.color = "green";
 
-document.getElementById("dog").nextElementSibling.style.color = "blue";
+//changing class name via child to 2nd item in list 1
+document.getElementByClassName("dog").
+
+//changing text value in last item in list1
+document.getElementById("fish").nextElementSibling.style.color = "blue";
+
+//event listener listening for event button and changing one value in embedded list as hidden
