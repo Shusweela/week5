706ffccf6192851cc90f360ad1129e234c3a50fd
shusweela <shusweela@gmailcom>
2020-09-10 22:21:18 -0500

    touching up event button

--- dom.js
+++ dom.js
@@ -1,12 +1,15 @@
-//Changes each element in first list of items. 
-document.getElementById("cat").style.color = "green";
+    //Changes each element in first list of items. 
+    document.getElementById("cat").style.color = "green";
 
-//changing text value in last item in list1
-document.getElementById("fish").nextElementSibling.style.color = "blue";
+    //changing text value in last item in list1
+    document.getElementById("fish").nextElementSibling.innerHTML = "goldfish";
 
-//changing class name via child to 2nd item in list 1
-document.getElementByclassName("dog").innerHTML = "canine";
+    //changing class name via child to 2nd item in list 1
+    document.getElementByclassName("dog").innerHTML = "canine";
 
 
 
-//event listener listening for event button and changing one value in embedded list as hidden
+    //event listener listening for event button and changing one value in embedded list as hidden
+    function myBtn (){
+        document.getElementById("bronze").style.visibility = "hidden";
+}
