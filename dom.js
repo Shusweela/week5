af2e793a234f16f87833e1036b7e44809742b7d4
shusweela <shusweela@gmailcom>
2020-09-10 20:18:04 -0500

    resolve js conflict to match prac up to date branch

--- dom.js
+++ dom.js
@@ -1,1 +1,12 @@
-document.getElementById("list1").style.color = "green";
+//Changes each element in first list of items. 
+document.getElementById("cat").style.color = "green";
+
+//changing text value in last item in list1
+document.getElementById("fish").nextElementSibling.style.color = "blue";
+
+//changing class name via child to 2nd item in list 1
+document.getElementByclassName("dog").innerHTML = "canine";
+
+
+
+//event listener listening for event button and changing one value in embedded list as hidden
